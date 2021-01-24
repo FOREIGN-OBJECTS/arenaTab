2d5576a52f18b875942498c738d88463a0ffac2e
Kallirroi <kallirroi.retzepi@gmail.com>
2021-01-24 11:51:41 -0500

    saving to localStorage, added simple dark mode toggling

--- src/newtab.ts
+++ src/newtab.ts
@@ -1,6 +1,9 @@
+let isDark = false
+
 chrome.runtime.sendMessage({ name: "fetchImage" }, (response) => {
-  // wait for response
+  // wait for response from API
   const { imageURL } = response
+
   document.getElementById('image1').style.backgroundImage = `url(${imageURL})`
 
   document.getElementById('image2').style.backgroundImage = `url(${imageURL})`
@@ -8,4 +11,33 @@ chrome.runtime.sendMessage({ name: "fetchImage" }, (response) => {
 
   document.getElementById('image3').style.backgroundImage = `url(${imageURL})`
   document.getElementById('image3').style.backgroundPosition = '75% 25%'
+
+  document.getElementById('input').addEventListener('change', e => handleChange(e))
+  document.getElementById('toggle').addEventListener('click', e => handleToggle(e))
+
 })
+
+const handleChange = (e: Event) => {
+  e.preventDefault()
+  const input = document.getElementById('input') as HTMLInputElement
+  const value = input.value
+  saveToLocalStorage(value)
+}
+
+const saveToLocalStorage = (value: string) => {
+  try {
+    const valueStr = JSON.stringify(value)
+    localStorage.setItem('input', valueStr)
+    console.log('saved', value, 'to localStorage')
+  }
+  catch(e) {
+    console.log('failed saving', e)
+  }
+}
+
+const handleToggle = (e: Event) => {
+  e.preventDefault()
+  isDark = !isDark
+  document.bgColor = isDark ? '#000' : '#fff'
+  document.getElementById('toggle').style.color = !isDark ? '#000' : '#fff'
+}
