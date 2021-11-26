1a87126560d6d8091549a382744ce9867781d513
Kallirroi <kallirroi.retzepi@gmail.com>
2021-11-26 18:07:10 -0500

    update webpack files

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,18 +1,19 @@
-const webpack = require("webpack");
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const srcDir = "../src/";
 var glob = require("glob");
 
 module.exports = {
-    entry: glob
+  entry: glob
         .sync(path.join(__dirname, srcDir + "*.ts"))
         .reduce(function (obj, el) {
             obj[path.parse(el).name] = el;
             return obj;
         }, {}),
     output: {
-        path: path.join(__dirname, "../dist/js"),
+        path: path.join(__dirname, "../dist"),
         filename: "[name].js",
     },
     optimization: {
@@ -21,24 +22,19 @@ module.exports = {
             chunks: "initial",
         },
     },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-    },
-    plugins: [
-        // exclude locale files in moment
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        new CopyPlugin({
-            patterns: [{ from: ".", to: "../", context: "public" }],
-            options: {},
-        }),
-    ],
+  module: {
+    rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ }], // do not forget to change/install your own TS loader
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: ".", to: "../", context: "public" }
+      ],
+    }),
+  ],
+  output: { filename: '[name].js', path: path.resolve(__dirname, '../dist') }, // chrome will look for files under dist/* folder
 };
